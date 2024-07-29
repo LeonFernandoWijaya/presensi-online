033051a58eb0a8ef142ffd637a7efd3e380c9e33
Leon <leon.825210088@stu.untar.ac.id>
2024-07-29 22:06:04 +0700

    repo
    repo

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <title>Laravel</title>
+
+    </head>
+    <body class="antialiased">
+    <div id="libur-container">
+
+    </div>
+    </body>
+    <script>
+        $.ajax({
+            url: 'https://dayoffapi.vercel.app/api',
+            type: 'GET',
+            success: function(data) {
+                data.forEach(function(item){
+                    console.log(item);
+                    let isNotCuti = item.is_cuti == false ? '' : 'Cuti Bersama';
+                    $('#libur-container').append('<div>'+item.keterangan+ ' ->' + isNotCuti+'</div>');
+                });
+            }
+        });
+    </script>
+</html>
