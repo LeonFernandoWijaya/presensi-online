47577596371d2ccc6fb049c98b372012fc380432
Leon <leon.825210088@stu.untar.ac.id>
2024-10-01 10:54:36 +0700

    tampilan mobile

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -23,104 +23,109 @@
     @auth
 
 
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
-        <div class="flex flex-wrap items-center md:justify-center justify-start md:gap-20 gap-0 mx-auto p-4 border-b border-gray-300">
-            {{-- <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <nav class="bg-white border-gray-200 dark:bg-gray-900">
+            <div
+                class="flex flex-wrap items-center md:justify-center justify-start md:gap-20 gap-0 mx-auto p-4 border-b border-gray-300">
+                {{-- <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
             </a> --}}
-            <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <button type="button" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
-                    data-dropdown-placement="bottom">
-                    <span class="sr-only">Open user menu</span>
-                    <div
-                        class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                        <span class="font-medium text-gray-600 dark:text-gray-300">{{ strtoupper(substr(Auth::user()->first_name, 0, 1)) }}@if(isset(Auth::user()->last_name)){{ strtoupper(substr(Auth::user()->last_name, 0, 1)) }}@endif</span>
+                <div
+                    class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse flex-row-reverse md:w-auto w-full justify-between">
+                    <button type="button" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
+                        data-dropdown-placement="bottom">
+                        <span class="sr-only">Open user menu</span>
+                        <div
+                            class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                            <span
+                                class="font-medium text-gray-600 dark:text-gray-300">{{ strtoupper(substr(Auth::user()->first_name, 0, 1)) }}{{ strtoupper(substr(Auth::user()->last_name, 0, 1)) ?? '' }}
+
+                            </span>
+                        </div>
+                    </button>
+                    <!-- Dropdown menu -->
+                    <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+                        id="user-dropdown">
+                        <ul class="py-2" aria-labelledby="user-menu-button">
+                            <li>
+                                <form action="{{ url('/logout') }}" method="POST">
+                                    @csrf
+                                    <button type="submit"
+                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
+                                        out</button>
+                                </form>
+                            </li>
+                        </ul>
                     </div>
-                </button>
-                <!-- Dropdown menu -->
-                <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-                    id="user-dropdown">
-                    <ul class="py-2" aria-labelledby="user-menu-button">
+                    <button data-collapse-toggle="navbar-user" type="button"
+                        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        aria-controls="navbar-user" aria-expanded="false">
+                        <span class="sr-only">Open main menu</span>
+                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 17 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M1 1h15M1 7h15M1 13h15" />
+                        </svg>
+                    </button>
+                </div>
+                <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
+                    <ul
+                        class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
-                            <form action="{{url('/logout')}}" method="POST">
-                                @csrf
-                                <button type="submit"
-                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
-                                    out</button>
-                            </form>
+                            <a href="{{ url('/presence') }}"
+                                class="block py-2 px-3 rounded md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700  md:dark:hover:bg-transparent dark:border-gray-700 {{ $navbar == 'presence' ? 'md:dark:text-blue-500 md:text-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:text-white' }}"
+                                aria-current="page">Presence</a>
+                        </li>
+                        <li>
+                            <a href="{{ url('/request') }}"
+                                class="block py-2 px-3 rounded md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700  md:dark:hover:bg-transparent dark:border-gray-700 {{ $navbar == 'request' ? 'md:dark:text-blue-500 md:text-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:text-white' }}">Request</a>
+                        </li>
+                        <li>
+                            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
+                                class="flex items-center justify-between w-full md:w-auto py-2 px-3 rounded md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700  md:dark:hover:bg-transparent dark:border-gray-700 {{ $navbar == 'history' ? 'md:dark:text-blue-500 md:text-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:text-white' }}">History
+                                <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    fill="none" viewBox="0 0 10 6">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="m1 1 4 4 4-4" />
+                                </svg></button>
+                            <!-- Dropdown menu -->
+                            <div id="dropdownNavbar"
+                                class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                                <ul class="py-2 text-sm text-gray-700 dark:text-gray-400"
+                                    aria-labelledby="dropdownLargeButton">
+                                    <li>
+                                        <a href="{{ url('/attendance-history') }}"
+                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Attendance
+                                            History</a>
+                                    </li>
+                                    <li>
+                                        <a href="{{ url('/overtime-history') }}"
+                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Overtime
+                                            History</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="{{ url('/reject') }}"
+                                class="block py-2 px-3 rounded md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700  md:dark:hover:bg-transparent dark:border-gray-700 {{ $navbar == 'reject' ? 'md:dark:text-blue-500 md:text-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:text-white' }}">Reject</a>
+                        </li>
+                        <li>
+                            <a href="{{ url('/user') }}"
+                                class="block py-2 px-3 rounded md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700  md:dark:hover:bg-transparent dark:border-gray-700 {{ $navbar == 'user' ? 'md:dark:text-blue-500 md:text-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:text-white' }}">User</a>
+                        </li>
+                        <li>
+                            <a href="{{ url('/holiday') }}"
+                                class="block py-2 px-3 rounded md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700  md:dark:hover:bg-transparent dark:border-gray-700 {{ $navbar == 'holiday' ? 'md:dark:text-blue-500 md:text-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:text-white' }}">Holiday</a>
+                        </li>
+                        <li>
+                            <a href="{{ url('/shift') }}"
+                                class="block py-2 px-3 rounded md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700  md:dark:hover:bg-transparent dark:border-gray-700 {{ $navbar == 'shift' ? 'md:dark:text-blue-500 md:text-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:text-white' }}">Shift</a>
                         </li>
                     </ul>
                 </div>
-                <button data-collapse-toggle="navbar-user" type="button"
-                    class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                    aria-controls="navbar-user" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 17 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M1 1h15M1 7h15M1 13h15" />
-                    </svg>
-                </button>
             </div>
-            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
-                <ul
-                    class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                    <li>
-                        <a href="{{url('/presence')}}"
-                            class="block py-2 px-3 rounded md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700  md:dark:hover:bg-transparent dark:border-gray-700 {{ ($navbar == 'presence') ? 'md:dark:text-blue-500 md:text-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:text-white' }}"
-                            aria-current="page">Presence</a>
-                    </li>
-                    <li>
-                        <a href="{{url('/request')}}"
-                            class="block py-2 px-3 rounded md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700  md:dark:hover:bg-transparent dark:border-gray-700 {{ ($navbar == 'request') ? 'md:dark:text-blue-500 md:text-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:text-white' }}">Request</a>
-                    </li>
-                    <li>
-                        <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
-                            class="flex items-center justify-between w-full md:w-auto py-2 px-3 rounded md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700  md:dark:hover:bg-transparent dark:border-gray-700 {{ ($navbar == 'history') ? 'md:dark:text-blue-500 md:text-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:text-white' }}">History
-                            <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                fill="none" viewBox="0 0 10 6">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" d="m1 1 4 4 4-4" />
-                            </svg></button>
-                        <!-- Dropdown menu -->
-                        <div id="dropdownNavbar"
-                            class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                            <ul class="py-2 text-sm text-gray-700 dark:text-gray-400"
-                                aria-labelledby="dropdownLargeButton">
-                                <li>
-                                    <a href="{{url('/attendance-history')}}"
-                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Attendance
-                                        History</a>
-                                </li>
-                                <li>
-                                    <a href="{{url('/overtime-history')}}"
-                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Overtime
-                                        History</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li>
-                        <a href="{{url('/reject')}}"
-                            class="block py-2 px-3 rounded md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700  md:dark:hover:bg-transparent dark:border-gray-700 {{ ($navbar == 'reject') ? 'md:dark:text-blue-500 md:text-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:text-white' }}">Reject</a>
-                    </li>
-                    <li>
-                        <a href="{{url('/user')}}"
-                            class="block py-2 px-3 rounded md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700  md:dark:hover:bg-transparent dark:border-gray-700 {{ ($navbar == 'user') ? 'md:dark:text-blue-500 md:text-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:text-white' }}">User</a>
-                    </li>
-                    <li>
-                        <a href="{{url('/holiday')}}"
-                            class="block py-2 px-3 rounded md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700  md:dark:hover:bg-transparent dark:border-gray-700 {{ ($navbar == 'holiday') ? 'md:dark:text-blue-500 md:text-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:text-white' }}">Holiday</a>
-                    </li>
-                    <li>
-                        <a href="{{url('/shift')}}"
-                            class="block py-2 px-3 rounded md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700  md:dark:hover:bg-transparent dark:border-gray-700 {{ ($navbar == 'shift') ? 'md:dark:text-blue-500 md:text-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:text-white' }}">Shift</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+        </nav>
 
     @endauth
 
