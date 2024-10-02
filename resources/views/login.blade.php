fe7f9d3663743a070639ff9068b2716060875811
Leon <leon.825210088@stu.untar.ac.id>
2024-10-02 20:10:20 +0700

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -17,8 +17,12 @@
             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password<span
                     class="text-red-500">*</span></label>
             <input type="password" name="password" id="password"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 mb-2"
                 placeholder="Enter your password" required="">
+            <div class="flex items-center mb-4">
+                <input id="show-password" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                <label for="show-password" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Show Password</label>
+            </div>
         </div>
         <div class="text-center">
             <button type="button" onclick="login()"
@@ -55,5 +59,13 @@
             }
         });
     }
+
+    $('#show-password').click(function() {
+        if ($(this).is(':checked')) {
+            $('#password').attr('type', 'text');
+        } else {
+            $('#password').attr('type', 'password');
+        }
+    });
 </script>
 @endsection
